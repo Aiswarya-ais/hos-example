c66c14412deaa826ff91e76c008489571f496a80
AISWARYA RAJEEV <98959500+Aiswarya-ais@users.noreply.github.com>
2025-09-26 16:50:48 +0530

    Update urls.py

--- django_tutorial/home/urls.py
+++ django_tutorial/home/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.index,name='home'),  
     path('about/', views.about,name='about'), 
     path('booking/', views.booking,name='booking'), 
-    path('doctors/', views.doctors,name='doctors'), 
+    path('doctors/', views.doctor,name='doctors'), 
     path('contact/', views.contact,name='contact'), 
     path('department/', views.department,name='department'),
 ]
+
