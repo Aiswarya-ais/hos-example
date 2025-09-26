d71e4609d3a13968e4bc816f3d70683d85f41707
AISWARYA RAJEEV <98959500+Aiswarya-ais@users.noreply.github.com>
2025-09-26 16:47:32 +0530

    Update models.py

--- django_tutorial/home/models.py
+++ django_tutorial/home/models.py
@@ -4,4 +4,12 @@ from django.db import models
 class department(models.Model):
     dep_name = models.CharField(max_length=100)
     dep_description=models.TextField()
+
+
+class doctors(models.Model):
+    doc_name= models.CharField(max_length=255)
+    doc_spec= models.CharField(max_length=255)
+    dep_name= models.ForeignKey(departments, on_delete=models.CASCADE)
+    doc_image= models.ImageField(upload_to='doctors')
+
     
