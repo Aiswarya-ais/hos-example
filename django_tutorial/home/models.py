56e66e16f114543c10dcfdcdd204d498d68ef00a
AISWARYA RAJEEV <98959500+Aiswarya-ais@users.noreply.github.com>
2025-09-26 08:13:22 +0530

    Add files via upload

---
+++ django_tutorial/home/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class department(models.Model):
+    dep_name = models.CharField(max_length=100)
+    dep_description=models.TextField()
+    
